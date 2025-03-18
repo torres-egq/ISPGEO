ff8c3cff7b2d47c15d0913939937a524382b23fd
Diego Torres <91717torres@gmail.com>
2025-03-18 13:39:49 -0300

    0

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ import { token } from './modules/token/token.service'; // Import the token funct
 dotenv.config();
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 3939;
