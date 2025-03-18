53033ce202e348896ad255823cc13863912b0ee4
Diego Torres <91717torres@gmail.com>
2025-03-18 11:40:12 -0300

    0

--- src/modules/token/token.service.ts
+++ src/modules/token/token.service.ts
@@ -11,7 +11,7 @@ export const token = async (req: Request, res: Response): Promise<any> => {
   console.log('teste teste teste');
   try {
     const token = generateToken({ user: 1 });
-    return res.status(200).json({ token });
+    return res.json(200).json({ token });
   } catch (error: any) {
     console.log(error);
     res.status(401).json({ error: error.message });
