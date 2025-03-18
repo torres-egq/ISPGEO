09d6c2b98db286b6ddf6f501a9319c23e443b796
Diego Torres <91717torres@gmail.com>
2025-03-18 11:47:04 -0300

    0

--- src/modules/token/token.service.ts
+++ src/modules/token/token.service.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
 const generateToken = (user: object): string => {
+  console.log(user);
   return jwt.sign(user, process.env.JWT_SECRET as string, {
     expiresIn: '1h',
   });
@@ -11,9 +12,9 @@ export const token = async (req: Request, res: Response): Promise<any> => {
   console.log('teste teste teste');
   try {
     const token = generateToken({ user: 1 });
-    return res.json(200).json({ token });
+    return res.status(200).json({ token }); // Corrigido: .json(200) -> .status(200)
   } catch (error: any) {
     console.log(error);
-    res.status(401).json({ error: error.message });
+    return res.status(401).json({ error: error.message }); // Adicionado return
   }
 };
