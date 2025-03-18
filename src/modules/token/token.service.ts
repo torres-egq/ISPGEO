0aae24dba659ae7c75aa7991813c186ccded72ad
Diego Torres <91717torres@gmail.com>
2025-03-18 16:17:35 -0300

    0.0.1

--- src/modules/token/token.service.ts
+++ src/modules/token/token.service.ts
@@ -13,7 +13,7 @@ export const token = async (req: Request, res: Response): Promise<any> => {
     // const { user } = req.body;
     // if (user === undefined || user === null || user === '')
     //   return res.status(401).json({ error: 'User is undefined' });
-
+    console.log(req.query);
     const token = generateToken(req.query);
     return res.status(200).json({ token }); // Corrigido: .json(200) -> .status(200)
   } catch (error: any) {
